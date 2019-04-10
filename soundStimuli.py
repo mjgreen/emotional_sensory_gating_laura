87e27d82992f22c6741c3a1cd653daf733f88bd0
matt <mgreen1718@gmail.com>
2019-04-10 12:18:51 +0100

    click 20ms and beep 20ms

--- soundStimuli.py
+++ soundStimuli.py
@@ -11,42 +11,50 @@ but this is yet to be confirmed.
 
 See the demo hardware > testSoundLatency.py
 """
-
-from __future__ import division
-from __future__ import print_function
-
 import sys
-from psychopy import logging, prefs
+from psychopy import logging, prefs, core
 logging.console.setLevel(logging.DEBUG)  # get messages about the sound lib as it loads
-
-from psychopy import sound, core
-
+prefs.general['audioLib'] = ['pygame']
+from psychopy import sound
 print('Using %s (with %s) for sounds' % (sound.audioLib, sound.audioDriver))
 
-highA = sound.Sound('A', octave=3, sampleRate=44100, secs=0.8, stereo=True)
-highA.setVolume(0.8)
-tick = sound.Sound(800, secs=0.01, sampleRate=44100, stereo=True)  # sample rate ignored because already set
-tock = sound.Sound('600', secs=0.01, sampleRate=44100, stereo=True)
+mybeep = sound.Sound('beep.wav')
+myclick = sound.Sound('click.wav')
 
-highA.play()
-core.wait(0.8)
-tick.play()
-core.wait(0.4)
-tock.play()
-core.wait(0.6)
+mybeep.play()
+core.wait(2)
 
-if sys.platform == 'win32':
-    ding = sound.Sound('ding')
-    ding.play()
+myclick.play()
+core.wait(1)
 
-    core.wait(1)
-
-    tada = sound.Sound('tada.wav')
-    tada.play()
-
-    core.wait(2)
-print('done')
-
+print('finished')
 core.quit()
+
+#
+#highA = sound.Sound('A', octave=3, sampleRate=44100, secs=0.8, stereo=True)
+#highA.setVolume(0.8)
+#tick = sound.Sound(800, secs=0.01, sampleRate=44100, stereo=True)  # sample rate ignored because already set
+#tock = sound.Sound('600', secs=0.01, sampleRate=44100, stereo=True)
+#
+#highA.play()
+#core.wait(0.8)
+#tick.play()
+#core.wait(0.4)
+#tock.play()
+#core.wait(0.6)
+
+#if sys.platform == 'win32':
+#    ding = sound.Sound('ding')
+#    ding.play()
+#
+#    core.wait(1)
+#
+#    tada = sound.Sound('beep.wav')
+#    tada.play()
+#
+#    core.wait(2)
+#print('done')
+
+#core.quit()
 
 # The contents of this file are in the public domain.
