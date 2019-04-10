239461e0630ce958dfcaef855fa5aa477fde49ff
Matt Green <mgreen1718@gmail.com>
2019-04-10 10:22:40 +0100

    got a runtime trial dict sorted

--- gating.py
+++ gating.py
@@ -1,5 +1,8 @@
 import os
 import datetime
+import random
+import shutil
+import pandas as pd
 from psychopy import visual, core, event, gui, parallel
 
 os.path.exists('/tmp/runtime-root') or os.mkdir('/tmp/runtime-root')
@@ -28,7 +31,48 @@ else:
     core.quit()
 
 # pull variables from the gui
-experiment_timestamp = dlg["Session timestamp:"]
+session_timestamp = dlg["Session timestamp:"]
 participant_number = dlg["Participant Number:"]
 mood_order_response = dlg["Order of mood induction:"]
 mood_order = ["angry", "neutral"] if mood_order_response == 'angry then neutral' else ["neutral", "angry"]
+
+# handle the results directory
+results_directory = os.path.join('results', 'P'+str(participant_number).zfill(3))
+if participant_number == 0 and os.path.exists(results_directory):
+    shutil.rmtree(results_directory)
+os.path.exists(results_directory) or os.makedirs(results_directory)
+
+# One of the blocks should have 5 click trials (out of 80) and the other should have 4 click trials (out of 80),
+# but we don't want to systematically have angry with 5 and neutral with 4 or the other way around,
+# so flip a coin to decide which block gets 4 clicks and which gets 5 clicks
+sound_list_4_clicks = ['click.wav' for sound in range(4)] + ['beep.wav' for sound in range(76)]
+random.shuffle(sound_list_4_clicks)
+sound_list_5_clicks = ['click.wav' for sound in range(5)] + ['beep.wav' for sound in range(75)]
+random.shuffle(sound_list_5_clicks)
+coin_flip = random.sample(['heads', 'tails'], 1)[0]
+sound_list = sound_list_4_clicks + sound_list_5_clicks if coin_flip == 'heads' else sound_list_5_clicks + sound_list_4_clicks  # sound_list has length 160
+
+# prepare list of trials, length 160, which is one full session made of 2 blocks of 80 trials
+n_blocks = 2
+n_trials_per_block = 80
+trial_dict = {}
+for bk in range(1, n_blocks + 1):
+    for tr in range(1, n_trials_per_block + 1):
+        instance = tr + ((bk - 1) * n_trials_per_block)
+        trial_dict[instance] = {
+            'participant_id': participant_number,
+            'session_timestamp': session_timestamp,
+            'unique_id': instance,
+            'block_number': bk,
+            'trial_number': tr,
+            'mood_level': mood_order[bk-1],
+            'sound_type': sound_list[instance-1]
+        }
+
+# write trial_dict to excel
+temp = pd.DataFrame.from_dict(trial_dict, orient='columns')
+temp = temp.transpose()
+print(temp.to_string(header=True, index=False, index_names=False, col_space=13, columns=['participant_id', 'session_timestamp', 'unique_id', 'block_number', 'trial_number', 'mood_level', 'sound_type']))
+writer = pd.ExcelWriter(os.path.join(results_directory, "P" + str(participant_number).zfill(3) + "_trials.xlsx"), engine='xlsxwriter')
+temp.to_excel(writer, str(participant_number), index=False, columns=['participant_id', 'session_timestamp', 'unique_id', 'block_number', 'trial_number', 'mood_level', 'sound_type'])
+writer.save()
