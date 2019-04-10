1f3cbbd6617b570af0e3b8d91357f1d30fb96ec9
matt <mgreen1718@gmail.com>
2019-04-11 00:33:14 +0100

    add audioswitcher - handy for testing what audio library we are using

---
+++ audioLibSwitcher.py
@@ -0,0 +1,26 @@
+from psychopy import prefs
+prefs.hardware['audioLib']=['pygame']
+from psychopy import sound
+sound.audioLib
+
+from psychopy import prefs
+prefs.hardware['audioLib']=['sounddevice']
+from psychopy import sound
+sound.audioLib
+
+from psychopy import prefs
+prefs.hardware['audioLib']=['pyo']
+from psychopy import sound
+sound.audioLib
+
+
+pip install \
+  --install-option="--use-coreaudio" \
+  --install-option "--use-double" \
+  git+git://github.c/usr/lib/python3/dist-packages/pyo.py
+
+
+/usr/lib/python3/dist-packages/pyo64.py
+/usr/lib/python3/dist-packages/pyolib
+/usr/lib/python3/dist-packages/pyo-0.9.1.egg-info
+om/belangeo/pyo.git
