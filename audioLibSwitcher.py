60b2eea19aa6f28e27361225f18452e5aaf1b272
matt <mgreen1718@gmail.com>
2019-04-11 02:29:50 +0100

    demonstration that we can load each of the 3 backends. Thats not to say we can use them after loading them though. Being at this stage is a lot better than getting errors when we try and load the libs cos it shows that they are correctly installed, and any remaining problems are essentially hardwareconfig

--- audioLibSwitcher.py
+++ audioLibSwitcher.py
@@ -1,26 +1,24 @@
-from psychopy import prefs
-prefs.hardware['audioLib']=['pygame']
-from psychopy import sound
-sound.audioLib
+lib = ['pygame']
+# lib = ['sounddevice']
+# lib = ['pyo']
 
 from psychopy import prefs
-prefs.hardware['audioLib']=['sounddevice']
+prefs.hardware['audioLib'] = lib
 from psychopy import sound
-sound.audioLib
-
-from psychopy import prefs
-prefs.hardware['audioLib']=['pyo']
-from psychopy import sound
-sound.audioLib
+print(sound.audioLib)
 
 
-pip install \
-  --install-option="--use-coreaudio" \
-  --install-option "--use-double" \
-  git+git://github.c/usr/lib/python3/dist-packages/pyo.py
-
-
-/usr/lib/python3/dist-packages/pyo64.py
-/usr/lib/python3/dist-packages/pyolib
-/usr/lib/python3/dist-packages/pyo-0.9.1.egg-info
-om/belangeo/pyo.git
+# from psychopy import prefs
+# prefs.hardware['audioLib']=['pygame']
+# from psychopy import sound
+# sound.audioLib
+#
+# from psychopy import prefs
+# prefs.hardware['audioLib']=['sounddevice']
+# from psychopy import sound
+# sound.audioLib
+#
+# from psychopy import prefs
+# prefs.hardware['audioLib']=['pyo']
+# from psychopy import sound
+# sound.audioLib
