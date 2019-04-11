fadcaf5cd552d5ccc5b7933a9743d33568fe8c31
matt <mgreen1718@gmail.com>
2019-04-11 05:40:40 +0100

    more efficient code

--- audioLibSwitcher.py
+++ audioLibSwitcher.py
@@ -1,24 +1,9 @@
 lib = ['pygame']
-# lib = ['sounddevice']
-# lib = ['pyo']
+lib = ['sounddevice']
+lib = ['pyo']
 
 from psychopy import prefs
 prefs.hardware['audioLib'] = lib
 from psychopy import sound
 print(sound.audioLib)
 
-
-# from psychopy import prefs
-# prefs.hardware['audioLib']=['pygame']
-# from psychopy import sound
-# sound.audioLib
-#
-# from psychopy import prefs
-# prefs.hardware['audioLib']=['sounddevice']
-# from psychopy import sound
-# sound.audioLib
-#
-# from psychopy import prefs
-# prefs.hardware['audioLib']=['pyo']
-# from psychopy import sound
-# sound.audioLib
