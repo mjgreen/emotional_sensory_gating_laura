61b9fb15703b36534486b7b48c285067fb64ba19
matt <mgreen1718@gmail.com>
2019-04-11 05:42:34 +0100

    rm apt version of pyo

--- soundStimuli.py
+++ soundStimuli.py
@@ -14,20 +14,22 @@ See the demo hardware > testSoundLatency.py
 import sys
 from psychopy import logging, prefs, core
 logging.console.setLevel(logging.DEBUG)  # get messages about the sound lib as it loads
-prefs.general['audioLib'] = ['sounddevice'] # 'pyo', 'pygame'
+# prefs.hardware['audioLib'] = ['pygame']
+# prefs.hardware['audioLib'] = ['sounddevice']
+prefs.hardware['audioLib'] = ['pyo']
 from psychopy import sound
 print('Using %s (with %s) for sounds' % (sound.audioLib, sound.audioDriver))
 
-mybeep = sound.Sound('beep_20ms.wav', volume=0.4)
-myclick = sound.Sound('click_20ms.wav', volume=1.0)
+mybeep = sound.Sound('beep_20ms.wav')
+myclick = sound.Sound('click_20ms.wav')
 
-core.wait(3)
+core.wait(1)
 
 mybeep.play()
-core.wait(.25)
+core.wait(.5)
 
 myclick.play()
-core.wait(.25)
+core.wait(.5)
 
 print('finished')
 core.quit()
