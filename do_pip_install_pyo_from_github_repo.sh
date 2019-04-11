03a6beaf484528a371053f5a71a24a1677a3ed18
matt <mgreen1718@gmail.com>
2019-04-11 02:33:16 +0100

    apparently you *can* pip install pyo, from the github

--- do_pip_install_pyo_from_github_repo.sh
+++ do_pip_install_pyo_from_github_repo.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+sudo apt install libsndfile-dev portaudio19-dev  # this pulls in jack0 and jack1 things and removes jack2 things
+
+pip install git+git://github.com/belangeo/pyo.git  # Successfully installed pyo-0.9.2
+
+## files are:
+#/home/matt/anaconda3/lib/python3.7/site-packages/pyo-0.9.2.dist-info
+#/home/matt/anaconda3/lib/python3.7/site-packages/pyo
+
+## previously, what I got from apt install python3-pyo was these files:
+#/usr/lib/python3/dist-packages/pyo.py
+#/usr/lib/python3/dist-packages/pyo64.py
+#/usr/lib/python3/dist-packages/pyolib
+#/usr/lib/python3/dist-packages/pyo-0.9.1.egg-info
