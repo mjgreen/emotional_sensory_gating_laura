4e335850179e740391062200c28ecc0c6833a4d2
matt <mgreen718@gmail.com>
2019-04-13 13:31:48 +0100

    less elegant list pop

--- gating_lite.py
+++ gating_lite.py
@@ -9,7 +9,7 @@ from contextlib import redirect_stdout
 with redirect_stdout(io.StringIO()): import psychopy
 from psychopy import logging, prefs
 logging.console.setLevel(logging.DEBUG)  # get messages about the sound lib as it loads
-sound_device_list = ["pyo", "pysoundcard", "sounddevice", "pygame"]
+sound_device_list = ["sounddevice", "pygame", "pyo", "pysoundcard"]
 prefs.hardware['audioLib'] = sound_device_list
 from psychopy import sound, core
 sound_info = 'Using %s (with %s) for sounds' % (sound.audioLib, sound.audioDriver)
@@ -17,24 +17,22 @@ sound_info = 'Using %s (with %s) for sounds' % (sound.audioLib, sound.audioDrive
 
 # load the sounds
 beep = sound.Sound('beep_20ms.wav')
-click = sound.Sound('click_20ms.wav')
+clik = sound.Sound('click_20ms.wav')
 
 
-def sound_trial(sound_type=None):
-    if sound_type is None:
-        return False
-    this_sound = click if trial_type == 'click' else beep
-    core.wait(2)
-    this_sound.play()
+def sound_trial(sound_type=None, trial_type=None):
+    print("psychopy sound info: {} {}".format(trial_type, sound_type))
+    core.wait(2.0)
+    sound_type.play()
     core.wait(0.5)
-    this_sound.play()
+    sound_type.play()
     core.wait(secs=random.uniform(5.0, 8.0))
 
 
-trial_order = random.sample([['beep', 'click'], ['click', 'beep']], 1)[0]
-msg = 'trial order is %s' % trial_order
+trial_order = random.sample([['beep', 'clik'], ['clik', 'beep']], 1)[0]
+sound_device = sound.audioLib
+msg = 'trial order is %s; sound device is %s' % (trial_order, sound_device)
 print(msg)
-logging.info(msg)
-for t in trial_order:
+for trial in range(len(trial_order)):  # trial_order:
     trial_type = trial_order.pop(0)
-    sound_trial(sound_type=trial_type)
+    sound_trial(sound_type=clik if trial_type == 'clik' else beep if trial_type == 'beep' else None, trial_type=trial_type)
