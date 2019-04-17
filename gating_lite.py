66215556db4e77c89bd04d258e4fe0fc0b53e605
Matt Green <matt>
2019-04-17 15:04:50 +0100

    after meeting laura but before deleting irrelevant sounds

--- gating_lite.py
+++ gating_lite.py
@@ -25,17 +25,15 @@ def sound_trial(sound_type=None, trial_type_name=None, trial_number=0, n_trials=
     sound_type.play()
     core.wait(0.5)
     print("{} # 2".format(trial_type_name))
-    if trial_type == 'beep':
-        beep2.play()
-    else:
-        sound_type.play()
+    sound_type.play()
     print("{} seconds silence\n".format(round(trial_final_silence, 1)))
     core.wait(secs=trial_final_silence)
 
 
-trial_order = random.sample([['beep', 'click'], ['click', 'beep']], 1)[0]
-for times in range(4):
-    trial_order = trial_order + random.sample([['beep', 'click'], ['click', 'beep']], 1)[0]
+# trial_order = random.sample([['beep', 'click'], ['click', 'beep']], 1)[0]
+trial_order = ['beep', 'beep', 'beep', 'beep']
+# for times in range(4):
+#     trial_order = trial_order + random.sample([['beep', 'click'], ['click', 'beep']], 1)[0]
 # trial_order = [random.sample([['beep', 'click'], ['click', 'beep']], 1)[0] for t in range(4)]
 msg = '\ntrial order is %s; sound device is %s, driver is %s\n' % (trial_order, sound.audioLib, '\"probably portaudio\"' if not sound.audioDriver else sound.audioDriver)
 print(msg)
@@ -45,11 +43,11 @@ num_trials = len(trial_order)
 for trial in range(len(trial_order)):  # trial_order:
     trial_type = trial_order.pop(0)
     # load the sounds here, else volume < 1.0 progressively makes the sound quieter every time it is .play()ed
-    beep = sound.Sound('beep_20ms.wav', volume=0.4)
-    beep2 = sound.Sound('beep_20ms.wav', volume=0.4)
-    clik = sound.Sound('click_20ms.wav', volume=1.0)
+    beep = sound.Sound('lijffijt_40ms_90dB_1000Hz_4ms_rise_fall.wav')
+    # beep2 = sound.Sound('beep_20ms.wav', volume=0.4)
+    # clik = sound.Sound('click_20ms.wav', volume=1.0)
     # go for trial
-    sound_trial(sound_type=clik if trial_type == 'click' else beep if trial_type == 'beep' else None, trial_type_name=trial_type, trial_number=trial, n_trials=num_trials,
-                trial_initial_silence=2.0, trial_final_silence=random.uniform(5.0, 8.0))
+    sound_trial(sound_type=beep if trial_type == 'beep' else None, trial_type_name=trial_type, trial_number=trial, n_trials=num_trials,
+                trial_initial_silence=2.0, trial_final_silence=random.uniform(6.0, 8.0))
 
 print('the end\n')
