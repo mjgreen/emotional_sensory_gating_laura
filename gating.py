147612c42f8fc4ed1a6544040e4180b8293f1920
matt <mgreen1718@gmail.com>
2019-04-20 19:07:02 +0100

    wrapped trial loop in try-catch

--- gating.py
+++ gating.py
@@ -60,41 +60,47 @@ def run():
     max_duration              =  2  # max_duration_total - fixed_duration
     beep1times = []
     beep2times = []
+    print("\nAbout to run {} trials now...\n".format(number_of_trials))
     for t in range(number_of_trials):
+        try:
 
-        # CORE TRIAL SEQUENCE
+            # CORE TRIAL SEQUENCE
 
-        time.sleep(silence_before_beeps)
+            time.sleep(silence_before_beeps)
 
-        beep1on = time.time()
-        beep.out(dur=beep_duration)
-        time.sleep(beep_duration)
-        beep1off = time.time()
+            beep1on = time.time()
+            beep.out(dur=beep_duration)
+            time.sleep(beep_duration)
+            beep1off = time.time()
 
-        time.sleep(silence_between_beeps)
+            time.sleep(silence_between_beeps)
 
-        beep2on = time.time()
-        beep.out(dur=beep_duration)
-        time.sleep(beep_duration)
-        beep2off = time.time()
+            beep2on = time.time()
+            beep.out(dur=beep_duration)
+            time.sleep(beep_duration)
+            beep2off = time.time()
 
-        time.sleep(random.uniform(min_duration, max_duration))
+            time.sleep(random.uniform(min_duration, max_duration))
 
-        # POST- TRIAL SEQUENCE
+            # POST- TRIAL SEQUENCE
 
-        beep1dur = 1000.0 * (beep1off - beep1on)
-        beep2dur = 1000.0 * (beep2off - beep2on)
+            beep1dur = 1000.0 * (beep1off - beep1on)
+            beep2dur = 1000.0 * (beep2off - beep2on)
 
-        beep1times.append(beep1dur)
-        beep2times.append(beep2dur)
+            beep1times.append(beep1dur)
+            beep2times.append(beep2dur)
 
-        with open("beep_times.dlm", "a") as f:
-            f.write("{:3s}  {:.12f}  {:.12f}\r\n".format(str(t+1).zfill(3), beep1dur, beep2dur))
+            with open("beep_times.dlm", "a") as f:
+                f.write("{:3s}  {:.12f}  {:.12f}\r\n".format(str(t+1).zfill(3), beep1dur, beep2dur))
 
-        print("trial {:3s}, beep 1 dur (ms) = {:.1f}; beep 2 dur (ms) = {:.1f}".format(str(t+1).zfill(3), beep1dur, beep2dur))
+            print("trial {:3s}, beep 1 dur (ms) = {:.1f}; beep 2 dur (ms) = {:.1f}".format(str(t+1).zfill(3), beep1dur, beep2dur))
 
-        if t in break_trials:
-            input('take a break: the experimenter will resume the experiment soon: >>')
-
+            if t in break_trials:
+                input('take a break: the experimenter will resume the experiment soon: >>')
+        except:
+            print("\nSomething went wrong, or user quit deliberately\n")
+            s.stop()
+            win.close()
     s.stop()
     win.close()
+# TODO send beep durations to matplotlib for a histogram
