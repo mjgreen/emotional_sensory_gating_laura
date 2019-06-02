f2c092259e0de45636db51104ce39414a6ce453d
mgreen <mgreen1718@gmail.com>
2019-06-02 10:29:18 +0100

    added parallel port stuff

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# TODO
-- [ ] add parallel port and send triggers
-- [ ] Per-trial trigger (11, 21); (12, 22); (13, 23), (14, 24). First number is which beep (1 or 2); second number is which block (1,2,3,4)
-- [ ] Block Number in output
-- [ ] Participant Number in output
-- [ ] Duration of post_beep_silence
-- [ ] Black Box test trigger-to-sound-onset latency and jitter 
-- [ ] Take a break stimulus to replace fixation cross during breaks
-- [ ] participant screen to control when they start
-- [ ] matplotlib / pylab histograms for latency and jitter
-
 # Laura RV's emotional sensory gating experiments
+Task: listen to the sounds.
 
-Task: listen to the sounds. 
+## TODO
+### important
+- [ ] participant screen to control when they start
+- [x] add parallel port and send triggers
+- [x] Per-trial trigger (11, 21); (12, 22); (13, 23), (14, 24). First number is which beep (1 or 2); second number is which block (1,2,3,4)
+- [x] Block Number in output
+- [x] Participant Number in output
+- [ ] "Take a break" stimulus to replace fixation cross during breaks
 
+### unimportant
+- [ ] Black Box test trigger-to-sound-onset latency and jitter 
+- [ ] matplotlib / pylab histograms for latency and jitter
--- gating.py
+++ gating.py
@@ -12,18 +12,43 @@ usage:
 """
 
 from pyo import Server, Sine  # SfPlayer # SfPlayer is for sound files; Sine is ok if we are generating our own sound from pyo itself
-from itertools import chain
 import time
-import csv
-import io
-import socket
 import random
-from contextlib import redirect_stdout
-#with redirect_stdout(io.StringIO()):
-from psychopy import visual, event, core
+import os
+import platform
+from psychopy import visual, event, core, monitors, parallel
+
+def get_parallel_port_address():
+    if platform.system() == 'Linux':
+        try:
+            parallel_port = parallel.ParallelPort(address='/dev/parport0')
+            print(parallel_port)
+        except OSError:
+            print("Parallel port cock-up: No such device or address: '/dev/parport0': " +
+                  "Did you run 'sudo rmmod lp ; sudo modprobe ppdev' yet?: " +
+                  "Did you do 'sudo adduser matt lp' to get access to the port?\n")
+            core.quit()
+        else:
+            there_is_a_parallel_port = True
+            return there_is_a_parallel_port, parallel_port
+    elif platform.system() == 'Windows':
+        try:
+            parallel_port = parallel.ParallelPort(address=0x1FF8)  # eeg z440
+            # psychopy.logging.flush()
+        except RuntimeError:
+            print("Parallel port cock-up:\n" +
+                  "On Windows, 64bit Python can't use inpout32's parallel port driver, which cocks everything up.\n" +
+                  "The solution is to use 32 bit python instead. Build psychopy using pip from the 32 bit python.\n" +
+                  "Make sure inpout32.dll is in the toplevel at runtime.\n")
+            there_is_a_parallel_port = False
+            parallel_port = None
+            return there_is_a_parallel_port, parallel_port
+        else:
+            there_is_a_parallel_port = True
+            return there_is_a_parallel_port, parallel_port
+
 
 def make_window():
-    # do all monitor and screen stuff
     viewpixx = monitors.Monitor("eeg", width=54, distance=53)
     viewpixx.setSizePix((1920, 1080))
     viewpixx.saveMon()
@@ -33,8 +58,8 @@ def make_window():
     xpx = monitor.getSizePix()[0]
     ypx = monitor.getSizePix()[1]
     xcm = monitor.getWidth()
-    win = visual.Window(fullscr=True, size=[xpx, ypx], units='pix', allowGUI=False, waitBlanking=True, color=[0, 0, 0], monitor=monitor.name, screen=which_screen, winType=None)
-    # win = visual.Window(fullscr=False, size=[800, 600], units='pix', allowGUI=True, waitBlanking=True, color=[0, 0, 0], monitor=monitor.name, screen=which_screen, winType=None)
+    # win = visual.Window(fullscr=True, size=[xpx, ypx], units='pix', allowGUI=False, waitBlanking=True, color=[0, 0, 0], monitor=monitor.name, screen=which_screen, winType=None)
+    win = visual.Window(fullscr=False, size=[800, 600], units='pix', allowGUI=True, waitBlanking=True, color=[0, 0, 0], monitor=monitor.name, screen=which_screen, winType=None)
     actual_frame_rate = win.getActualFrameRate()
     print("actual frame rate: {} Hz".format(int(round(actual_frame_rate))))
     extent = 10
@@ -55,74 +80,83 @@ def construct_sound(beep_hz=1000.0):
     beep = Sine(freq=[beep_hz, beep_hz]).play()
     return beep
 
-def run():
-    try:
-        # print("\n")  # just to start console output with a blank line
-        with open("beep_times.dlm", "w+") as f:
-            f.write("trl  beep_1_duration  beep_2_duration\r\n")
-        win                       =  make_window()
-        print("  matt: Starting the sound server up now: warnings will be suppressed...")
-#        with redirect_stdout(io.StringIO()):
-        s                         =  start_sound_server()
-        beep                      =  construct_sound(beep_hz=1000.0)
-        number_of_trials          =  160
-        break_trials              =  [39, 79, 119]
-        # DURATIONS, IN SECONDS
-        silence_before_beeps      =    2.000
-        beep_duration             =    0.010  # Laura = 0.010 as of 23 May (was previously 0.040)
-        silence_between_beeps     =    0.500
-        fixed_duration            =  silence_before_beeps + beep_duration + silence_between_beeps + beep_duration
-        min_duration              =  6.0  # min_duration_total - fixed_duration # Laura = 6 seconds
-        max_duration              =  8.0  # max_duration_total - fixed_duration # Laura = 8 seconds
-        beep1times = []
-        beep2times = []
-        print("  matt: About to run {} trials now...".format(number_of_trials))
-        for t in range(number_of_trials):
-
-            # CORE TRIAL SEQUENCE
-
-            time.sleep(silence_before_beeps)
-
-            beep1on = time.time()
-            beep.out(dur=beep_duration)
-            time.sleep(beep_duration)
-            beep1off = time.time()
-
-            time.sleep(silence_between_beeps)
-
-            beep2on = time.time()
-            beep.out(dur=beep_duration)
-            time.sleep(beep_duration)
-            beep2off = time.time()
-
-            post_beep_silence_dur = random.uniform(min_duration, max_duration)
-            t0 = core.getTime()
-            while core.getTime() - t0 < post_beep_silence_dur:
-                k = event.getKeys(keyList=['escape'])
-                if k:
-                    print("quitting because experimenter pressed escape")
-                    win.close()
-                    core.quit()
-
-            # POST- TRIAL SEQUENCE
-
-            beep1dur = 1000.0 * (beep1off - beep1on)
-            beep2dur = 1000.0 * (beep2off - beep2on)
-
-            beep1times.append(beep1dur)
-            beep2times.append(beep2dur)
-
-            with open("beep_times.dlm", "a") as f:
-                f.write("{:3s}  {:.12f}  {:.12f}\r\n".format(str(t+1).zfill(3), beep1dur, beep2dur))
-
-            print("  matt: trial {:3s}: pre-beeps = {}; beep 1 = {:.1f}; inter-beeps = {}; beep 2 = {:.1f}, post-beeps = {:.1f}".format(str(t+1).zfill(3), int(1000.0*silence_before_beeps), beep1dur, int(1000.0*silence_between_beeps), beep2dur, 1000.0*post_beep_silence_dur))
-
-            if t in break_trials:
-                input('\ttake a break: the experimenter will resume the experiment soon: >>')
-    except:
-        print("\n  matt : Something went wrong, or user quit deliberately\n")
-        s.stop()
-        win.close()
+def graceful_exit(s, win):
     s.stop()
     win.close()
-# TODO send beep durations to matplotlib for a histogram
+    core.quit()
+
+def get_participant_number():
+    participant_number = input("participant number: ")
+    return participant_number
+
+def run():
+    participant_number = get_participant_number()
+    os.path.isdir("results") or os.mkdir("results")
+    results_directory = os.path.join("results", str(participant_number).zfill(3))
+    os.path.isdir(results_directory) or os.mkdir(results_directory)
+    results_file = str(participant_number).zfill(3) + "_results.dlm.csv"
+    results_path = os.path.join(results_directory, results_file)
+    trial_info_header = "participant\tblock\ttrl\tprebeep\tbeep1\tinterbeep\tbeep2\tpostbeeps\r\n"
+    with open(results_path, "a") as f:
+        f.write(trial_info_header)
+    parallel_port = get_parallel_port_address()
+    win = make_window()
+    s = start_sound_server()
+    beep = construct_sound(beep_hz=1000.0)
+    number_of_trials = 160
+    break_trials = [39, 79, 119]
+    silence_before_beeps = 2.000
+    beep_duration = 0.010  # Laura = 0.010 as of 23 May (was previously 0.040)
+    silence_between_beeps = 0.500
+    fixed_duration = silence_before_beeps + beep_duration + silence_between_beeps + beep_duration
+    min_duration = 6.0  # min_duration_total - fixed_duration # Laura = 6 seconds
+    max_duration = 8.0  # max_duration_total - fixed_duration # Laura = 8 seconds
+    block_number = 1
+    for t in range(number_of_trials):
+
+        parallel_port.setData(0)
+
+        time.sleep(silence_before_beeps)
+
+        beep_number = 1
+        trigger_code = (beep_number*10) + block_number
+        beep1on = time.time()
+        parallel_port.setData(trigger_code)
+        beep.out(dur=beep_duration)
+        time.sleep(beep_duration)
+        beep1off = time.time()
+
+        time.sleep(silence_between_beeps)
+
+        beep_number = 2
+        trigger_code = (beep_number * 10) + block_number
+        beep2on = time.time()
+        parallel_port.setData(trigger_code)
+        beep.out(dur=beep_duration)
+        time.sleep(beep_duration)
+        beep2off = time.time()
+
+        post_beep_silence_dur = random.uniform(min_duration, max_duration)
+        t0 = core.getTime()
+        while core.getTime() - t0 < post_beep_silence_dur:
+            k = event.getKeys(keyList=['escape'])
+            if k:
+                print("quitting because experimenter pressed escape")
+                win.close()
+                core.quit()
+
+        trial_info = "{}\t{}\t{:3s}\t{}\t{:.12f}\t{}\t{:.12f}\t{}\r\n".format(participant_number, block_number, str(t+1).zfill(3), 1000.0*silence_before_beeps, 1000.0*(beep1off-beep1on), 1000.0*silence_between_beeps, 1000.0*(beep2off-beep2on), 1000.0*post_beep_silence_dur)
+
+        with open(results_path, "a") as f:
+            f.write(trial_info)
+
+        print(trial_info)
+
+        if t in break_trials:
+            input('\ttake a break: the experimenter will resume the experiment soon: >>')
+            block_number += 1
+
+    # number_of_trials has been reached
+    s.stop()
+    win.close()
+    core.quit()
