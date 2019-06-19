f4a2886ec9c0be75e7294d0b09f2ebc016f2c5b8
mgreen <mgreen1718@gmail.com>
2019-06-19 14:18:53 +0100

    works on windows assuming pyo is properly installed

--- gating_no_triggers.py
+++ gating_no_triggers.py
@@ -81,11 +81,17 @@ def do_fix_cross(win):
     win.flip()
 
 def start_sound_server():
-    s = Server(sr=48000, nchnls=2, buffersize=2048, duplex=0, audio='jack', jackname='pyo')
-    s.setOutputDevice(0)
-    s.deactivateMidi()
-    s.boot()
-    s.start()
+    # This stuff is windows specific
+    host = "mme"
+    s = Server(duplex=0)
+    s.reinit(buffersize=1024, duplex=0, winhost=host)
+    s.boot().start()
+    # This stuff is linux specific
+    # s = Server(sr=48000, nchnls=2, buffersize=2048, duplex=0, audio='jack', jackname='pyo')
+    # s.setOutputDevice(0)
+    # s.deactivateMidi()
+    # s.boot()
+    # s.start()
     return s
 
 def construct_sound(beep_hz=1000.0):
@@ -108,76 +114,76 @@ def get_participant_number():
     participant_number = ok_data[0]
     return participant_number
 
-def run():
-    participant_number = get_participant_number()
-    os.path.isdir("results") or os.mkdir("results")
-    results_directory = os.path.join("results", str(participant_number).zfill(3))
-    os.path.isdir(results_directory) or os.mkdir(results_directory)
-    results_file = str(participant_number).zfill(3) + "_results.dlm.csv"
-    results_path = os.path.join(results_directory, results_file)
-    trial_info_header = "participant\tblock\ttrl\tprebeep\tbeep1\tinterbeep\tbeep2\tpostbeeps\r\n"
+
+participant_number = get_participant_number()
+os.path.isdir("results") or os.mkdir("results")
+results_directory = os.path.join("results", str(participant_number).zfill(3))
+os.path.isdir(results_directory) or os.mkdir(results_directory)
+results_file = str(participant_number).zfill(3) + "_results.dlm.csv"
+results_path = os.path.join(results_directory, results_file)
+trial_info_header = "participant\tblock\ttrl\tprebeep\tbeep1\tinterbeep\tbeep2\tpostbeeps\r\n"
+with open(results_path, "a") as f:
+    f.write(trial_info_header)
+# parallel_port = get_parallel_port_address()
+win = make_window()
+s = start_sound_server()
+beep = construct_sound(beep_hz=1000.0)
+number_of_trials = 160
+break_trials = [39, 79, 119]
+silence_before_beeps = 2.000
+beep_duration = 0.010  # Laura = 0.010 as of 23 May (was previously 0.040)
+silence_between_beeps = 0.500
+min_duration = 6.0  # Laura = 6 seconds
+max_duration = 8.0  # Laura = 8 seconds
+block_number = 1
+press_any_key_when_ready(win)
+for t in range(number_of_trials):
+
+    do_fix_cross(win)
+
+    # parallel_port.setData(0)
+
+    time.sleep(silence_before_beeps)
+
+    beep_number = 1
+    trigger_code = (beep_number*10) + block_number
+    beep1on = time.time()
+    # parallel_port.setData(trigger_code)
+    beep.out(dur=beep_duration)
+    time.sleep(beep_duration)
+    beep1off = time.time()
+
+    time.sleep(silence_between_beeps)
+
+    beep_number = 2
+    trigger_code = (beep_number * 10) + block_number
+    beep2on = time.time()
+    # parallel_port.setData(trigger_code)
+    beep.out(dur=beep_duration)
+    time.sleep(beep_duration)
+    beep2off = time.time()
+
+    post_beep_silence_dur = random.uniform(min_duration, max_duration)
+    t0 = core.getTime()
+    while core.getTime() - t0 < post_beep_silence_dur:
+        k = event.getKeys(keyList=['escape'])
+        if k:
+            print("quitting because experimenter pressed escape")
+            win.close()
+            core.quit()
+
+    trial_info = "{}\t{}\t{:3s}\t{}\t{:.12f}\t{}\t{:.12f}\t{}\r\n".format(participant_number, block_number, str(t+1).zfill(3), 1000.0*silence_before_beeps, 1000.0*(beep1off-beep1on), 1000.0*silence_between_beeps, 1000.0*(beep2off-beep2on), 1000.0*post_beep_silence_dur)
+
     with open(results_path, "a") as f:
-        f.write(trial_info_header)
-    # parallel_port = get_parallel_port_address()
-    win = make_window()
-    s = start_sound_server()
-    beep = construct_sound(beep_hz=1000.0)
-    number_of_trials = 160
-    break_trials = [39, 79, 119]
-    silence_before_beeps = 2.000
-    beep_duration = 0.010  # Laura = 0.010 as of 23 May (was previously 0.040)
-    silence_between_beeps = 0.500
-    min_duration = 6.0  # Laura = 6 seconds
-    max_duration = 8.0  # Laura = 8 seconds
-    block_number = 1
-    press_any_key_when_ready(win)
-    for t in range(number_of_trials):
+        f.write(trial_info)
 
-        do_fix_cross(win)
+    print(trial_info)
 
-        # parallel_port.setData(0)
+    if t in break_trials:
+        take_a_break(win)
+        block_number += 1
 
-        time.sleep(silence_before_beeps)
-
-        beep_number = 1
-        trigger_code = (beep_number*10) + block_number
-        beep1on = time.time()
-        # parallel_port.setData(trigger_code)
-        beep.out(dur=beep_duration)
-        time.sleep(beep_duration)
-        beep1off = time.time()
-
-        time.sleep(silence_between_beeps)
-
-        beep_number = 2
-        trigger_code = (beep_number * 10) + block_number
-        beep2on = time.time()
-        # parallel_port.setData(trigger_code)
-        beep.out(dur=beep_duration)
-        time.sleep(beep_duration)
-        beep2off = time.time()
-
-        post_beep_silence_dur = random.uniform(min_duration, max_duration)
-        t0 = core.getTime()
-        while core.getTime() - t0 < post_beep_silence_dur:
-            k = event.getKeys(keyList=['escape'])
-            if k:
-                print("quitting because experimenter pressed escape")
-                win.close()
-                core.quit()
-
-        trial_info = "{}\t{}\t{:3s}\t{}\t{:.12f}\t{}\t{:.12f}\t{}\r\n".format(participant_number, block_number, str(t+1).zfill(3), 1000.0*silence_before_beeps, 1000.0*(beep1off-beep1on), 1000.0*silence_between_beeps, 1000.0*(beep2off-beep2on), 1000.0*post_beep_silence_dur)
-
-        with open(results_path, "a") as f:
-            f.write(trial_info)
-
-        print(trial_info)
-
-        if t in break_trials:
-            take_a_break(win)
-            block_number += 1
-
-    # number_of_trials has been reached
-    s.stop()
-    win.close()
-    core.quit()
+# number_of_trials has been reached
+s.stop()
+win.close()
+core.quit()
